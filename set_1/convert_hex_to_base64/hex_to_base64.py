0ef80ad7079217cb6bb0874bba1a92f49e5a0ca3
Alexey Beliaev <abeliaev@ascendix.net>
2017-09-28 14:08:40 +0300

    Finished first challenge

---
+++ set_1/convert_hex_to_base64/hex_to_base64.py
@@ -0,0 +1,7 @@
+import base64
+
+input_hex_value = b"49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+output_base64_value = base64.b64encode(input_hex_value)
+
+print(input_hex_value)
+print(output_base64_value)
